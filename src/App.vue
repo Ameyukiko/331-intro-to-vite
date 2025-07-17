1e62d7d86f5557b72c19a30656df41bfb3541f02
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-07-17 23:06:13 +0700

    Complete Lab3 1.10

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Event</RouterLink> | <RouterLink to="/about">About</RouterLink> |
-          <RouterLink to="/student">Student</RouterLink>
+          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink :to="{ name: 'student-view' }">Student</RouterLink>
         </nav>
       </div>
     </header>
@@ -19,23 +20,23 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style>
 #layout {
-  font-family: Avenir, Helvetica, Arial ,sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-nav{
+nav {
   padding: 30px;
 }
-nav a{
+nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 nav a.router-link-exact-active {
   color: #42b983;
 }
-h2{
+h2 {
   font-size: 20px;
 }
 </style>
