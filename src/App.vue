8da65d5c6a9c40c63345c7d2003fc924f325f26e
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-07-31 21:04:03 +0700

    10

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,14 @@ const { message } = storeToRefs(store)
 <template>
   <div class="text-center font-sans text-gray-700 antialiased">
     <header>
-     <div id="flashMessage" v-if="message">
+     <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
         <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
-          <RouterLink :to="{ name: 'student-view' }">Student</RouterLink>
+          <RouterLink  class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink  class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink  class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'student-view' }">Student</RouterLink>
         </nav>
       </div>
     </header>
@@ -26,28 +26,7 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-nav {
-  padding: 30px;
-}
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 h2 {
   font-size: 20px;
 }
-@keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to{
-    background-color: transparent;
-  }
-}
-  #flashMessage {
-    animation: yellofade 3s ease-in-out;
-  }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ export default {
     extend: {
       boxShadow: {
         'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
-      }
+      },
+      fontFamily: {
+        sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      // animation class
+      animation: {
+        fade: 'yellowfade 3s ease-in-out;',
+      },
+      keyframes: {
+        yellowfade: {
+          '0%': { backgroundColor: 'yellow' },
+          '100%': { backgroundColor: 'transparent' },
+        },
+      },
     },
   },
   plugins: [],
