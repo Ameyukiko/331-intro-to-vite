eb0c4242b970dafb08401b3f26167ba180c1c2ec
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-10-01 00:05:47 +0700

    6.4

--- src/views/event/EventFromView.vue
+++ src/views/event/EventFromView.vue
@@ -2,10 +2,11 @@
 import EventService from '@/Services/EventService'
 import BaseInput from '@/components/BaseInput.vue'
 import { useMessageStore } from '@/stores/message'
-import type { EventItem } from '@/types'
-import { ref } from 'vue'
+import type { EventItem, Organizer } from '@/types'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import imageUpload from '@/components/ImageUpload.vue'
+import OrganizerSerivce from '@/Services/OrganizerService'
 const event = ref<EventItem>({
   category: '',
   title: '',
@@ -19,6 +20,20 @@ const event = ref<EventItem>({
 })
 const router = useRouter()
 const store = useMessageStore()
+// ✅ ตัวแปร organizers
+const organizers = ref<Organizer[]>([])
+
+// ✅ โหลด organizers ตอน mount
+onMounted(() => {
+  OrganizerSerivce.getOrganizers()
+    .then((response) => {
+      organizers.value = response.data
+    })
+    .catch(() => {
+      router.push({ name: 'network-error-view' })
+    })
+})
+
 function saveEvent() {
   EventService.saveEvent(event.value)
     .then((response) => {
@@ -31,6 +46,7 @@ function saveEvent() {
     .catch(() => {
       router.push({ name: 'network-error-view' })
     })
+
 }
 </script>
 
@@ -47,6 +63,21 @@ function saveEvent() {
       <BaseInput v-model="event.description" label="Description" type="text" placeholder="Description" />
       <label>Location</label>
       <BaseInput v-model="event.location" label="Location" type="text" placeholder="Location" />
+
+        <BaseInput v-model="event.location" type="text" label="Location" />
+  <h3>Who is your organizer?</h3>
+  <label>Select an Organizer</label>
+  <select v-model="event.organizer.id">
+    <option
+      v-for="option in organizers"
+      :value="option.id"
+      :key="option.id"
+      :selected="option.id === event.organizer.id"
+    >
+      {{ option.name }}
+    </option>
+  </select>
+  
       <h3> The image of the Event</h3>
       <image-upload v-model="event.image" />
       <button class="button" type="submit">Submit</button>
