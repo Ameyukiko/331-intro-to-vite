416d6b80a6d79abb17fce4b06da5d8620dfc906e
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-10-03 04:19:40 +0700

    4.2

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,98 +1,68 @@
 <script setup lang="ts">
-import InputText from '@/components/InputText.vue'
+import InputText from '../components/InputText.vue'
 import * as yup from 'yup'
 import { useField, useForm } from 'vee-validate'
+import { useAuthStore } from '@/stores/auth'
 
-// กำหนด validation schema
+const authStore = useAuthStore()
+
 const schema = yup.object({
-  email: yup
-    .string()
-    .required('The email is required')
-    .email('Input must be a valid email.'),
-  password: yup
-    .string()
-    .required('The password is required')
-    .min(6, 'The password must be at least 6 characters.')
+  email: yup.string().required('The email is required'),
+  password: yup.string().required('The password is required')
 })
 
-// init form
 const { errors, handleSubmit } = useForm({
   validationSchema: schema,
   initialValues: { email: '', password: '' }
 })
 
-// สร้าง field
 const { value: email } = useField<string>('email')
 const { value: password } = useField<string>('password')
 
-// submit function
 const onSubmit = handleSubmit((values) => {
-  console.log('login submit', values)
+  authStore.login(values.email, values.password)
 })
 </script>
 
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <!-- Header -->
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        class="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
-      />
+      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company" />
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Sign in to your account
       </h2>
     </div>
 
-    <!-- Form -->
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" @submit.prevent="onSubmit">
-        <!-- Email -->
         <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <label class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <InputText
-              id="email"
-              v-model="email"
-              type="email"
-              placeholder="you@example.com"
-              :error="errors.email"
-            />
+            <InputText v-model="email" type="text" placeholder="you@example.com" :error="errors.email" />
           </div>
         </div>
 
-        <!-- Password -->
         <div>
           <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            <label class="block text-sm font-medium leading-6 text-gray-900">Password</label>
             <div class="text-sm">
               <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
             </div>
           </div>
           <div class="mt-2">
-            <InputText
-              id="password"
-              v-model="password"
-              type="password"
-              placeholder="••••••••"
-              :error="errors.password"
-            />
+            <InputText v-model="password" type="password" placeholder="••••••••" :error="errors.password" />
           </div>
         </div>
 
-        <!-- Submit Button -->
         <div>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
+          <button type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Sign in
           </button>
         </div>
       </form>
 
-      <!-- Footer -->
       <p class="mt-10 text-center text-sm text-gray-500">
         Not a member?
         <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Try to register here</a>
